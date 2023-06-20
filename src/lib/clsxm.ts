1828a4a102ccfa57876ddeebe8fb6fc2d2bc17ff
ethan <72283335+intuitiveen@users.noreply.github.com>
2023-06-19 20:56:31 -0400

    cypress stuff

--- src/lib/clsxm.ts
+++
@@ -1,8 +0,0 @@
-import type { ClassValue } from "clsx";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/** Merge classes with tailwind-merge with clsx full feature */
-export default function clsxm(...classes: ClassValue[]) {
-    return twMerge(clsx(...classes));
-}
