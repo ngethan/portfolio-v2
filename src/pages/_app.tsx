de254e50dd5ce02af28061384e8af34037171116
ethan <72283335+intuitiveen@users.noreply.github.com>
2023-06-27 01:02:42 -0400

    yay!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,12 @@ import "../styles/global.css";
 import type { AppProps } from "next/app";
 
 import Cursor from "@/components/Cursor";
-import { GoogleTagManager } from "@/lib/GoogleTagManager";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <Component {...pageProps} />
             <Cursor />
-            <GoogleTagManager
-                containerId={
-                    process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string
-                }
-            />
         </>
     );
 };
